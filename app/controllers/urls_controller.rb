3320d2d18b425c72c40a5a06c2d7444ff92e46ce
jjkazm <jjkazm@gmail.com>
2018-11-08 13:15:11 +0100

    Create urls controller

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,7 +1,23 @@
 class UrlsController < ApplicationController
   def new
+    @url = Url.new
+  end
+
+  def create
+    @url = Url.new(url_params)
   end
 
   def show
+    @url = Url.find_by(short: params[:short])
+    redirect_to "https://#{@url.long}"
   end
+
+  def done
+  end
+
+  private
+    def url_params
+      params.require(:url).permit(:long)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'urls/new'
-  get 'urls/show'
+  get '/', to: "urls#new"
+  get '/:short', to: "urls#show", param: :short
+  get 'done/:short', to: "urls#done"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/", to: "url#show"
+
 
 end
