1eeecdbde9e1a1dabe6e9f37bd33efc6b7ba4de9
jjkazm <jjkazm@gmail.com>
2018-11-13 19:48:06 +0100

    Add more shortening url feature tests

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -23,7 +23,7 @@ class UrlsController < ApplicationController
 
   def show
     @url = Url.find_by(short: params[:short])
-    redirect_to "http://#{@url.long}"
+    redirect_to "https://#{@url.long}"
   end
 
   def done
--- spec/features/shortening_url_spec.rb
+++ spec/features/shortening_url_spec.rb
@@ -1,13 +1,26 @@
 require "rails_helper"
 
-RSpec.feature "Shortening url" do
-
-  scenario "is succesfull" do
+RSpec.feature "Shortening url", js:true do
+  scenario "succesfully directs to done page" do
     visit "/"
     fill_in "url[long]", with: "www.google.com"
     click_button "Shorten"
+    expect(current_url).to have_content("done")
     expect(page).to have_content("google.com")
     expect(page).not_to have_content("www.google.com")
+    new_window = window_opened_by { click_link "localhost" }
+    within_window new_window do
+      expect(current_url).to eq "https://www.google.com/"
+    end
+  end
+
+  scenario "creates 8 character link" do
+    visit "/"
+    fill_in "url[long]", with: "www.google.com"
+    click_button "Shorten"
+    short_url = find_by_id("short")
+    puts short_url.text.length
+    expect(short_url.text.length - "http://localhost:3000/".length).to eq 8
   end
 
   scenario "fails with empty url provided" do
